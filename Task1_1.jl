b7c4655c6416219bc6f738d66b51d928749643c2
alex-schonrock <alex@schonrocks.com>
2025-04-07 16:22:28 +0200

    started scenario generator

--- Task1_1.jl
+++ Task1_1.jl
@@ -6,10 +6,19 @@ data = CSV.read("Task1_1.csv", DataFrame)
 p_real = CSV.read("WindFarmData.csv", DataFrame;  delim=';')
 ### IMPORTANT: df_DA is not with correct data ###
 lambda_DA = CSV.read("DA_price.csv", DataFrame;  delim=';')
+
+
 println(p_real)
 
+function scenario_generator(no_of_scenarios)
+    production_index = 
+    DA_price_index = 
+    system_status_index = 
+    return p_real[production_index], lambda_DA[DA_price_index], system_status[system_status_index]
+end
 
-function optimise_bidding_quantity(system_status, p_real, lambda_DA)
+
+function optimise_bidding_quantity(system_status, p_real, lambda_DA) # later add the input: "pricing_scheme", where it will be either "one-price" or "two-price"
     m = Model(GLPK.Optimizer)
 
     t = 1:length(p_real)
@@ -31,11 +40,12 @@ function optimise_bidding_quantity(system_status, p_real, lambda_DA)
     if system_status == "deficit"
         up_price = 1.25 * lambda_DA[t]
         down_price = 1.25 * lambda_DA[t]
-    end
-    if system_status == "excess"
+    elseif system_status == "excess"
         up_price = 0.85 * lambda_DA[t]
         down_price = 0.85 * lambda_DA[t]
+    else
+        println("Error: system_status must be either 'deficit' or 'excess'")
     end
 
     @objective(m, Max, sum(lambda_DA[k]*W[k]+up_price*lambda_DA[k]*t_up[k]-down_price*lambda_DA[k]*t_down[k] for k in t)) 
-
+end
