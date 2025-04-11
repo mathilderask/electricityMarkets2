b84037bd8dfdfc10beb20198ee4a07c602084439
alex-schonrock <alex@schonrocks.com>
2025-04-11 14:30:02 +0200

    Random scenario generator now works!

--- Task1_1.jl
+++ Task1_1.jl
@@ -2,7 +2,6 @@ import Pkg, JuMP, GLPK, DataFrames, CSV, Plots, Random
 using Pkg, JuMP, GLPK, DataFrames, CSV, Plots, Random
 
 p_real_DF = CSV.read("windscenarios_zone2.csv", DataFrame;  delim=',')
-### IMPORTANT: df_DA is not with correct data ###
 lambda_DA_DF = CSV.read("DA_hourly_price_scenarios.csv", DataFrame;  delim=',')
 scenarios_DF = CSV.read("scenario_combinations.csv", DataFrame; delim=',')
 system_status_DF = CSV.read("power_system_conditions.csv", DataFrame; delim=',')
@@ -11,14 +10,25 @@ scenario_ID = scenarios_DF[:, 1]
 
 
 function scenario_generator(no_of_scenarios)
+#### Returns a random selection of defined number of scenarios in.
+#### Returns 3 dataframes for the three variables where each column is corresponding data for a randomly selected scenario
+#### Each dataframe has 24 rows for the 24 hours of the day
     scenarios_indices = randperm(length(scenario_ID))[1:no_of_scenarios]
-    #print(scenarios_indices)
-    p_real_indices = p_real_DF[scenarios_indices, 2]
-    print(p_real_indices)
-    lambda_DA_indices = 
-    system_status_indices = 
-    #return p_real, lamda_DA, system_status
-    return p_real_indices
+    p_real_indices = scenarios_DF[scenarios_indices, 2]
+    lambda_DA_indices = scenarios_DF[scenarios_indices, 3]
+    system_status_indices = scenarios_DF[scenarios_indices, 4]
+
+    p_real_cols = [p_real_DF[:, i+1] for i in p_real_indices]
+    col_names = Symbol.("selected_scenario_", 1:length(p_real_cols))  # custom unique names
+    p_real = DataFrame(p_real_cols, col_names)
+
+    lambda_DA_cols = [lambda_DA_DF[:, i+1] for i in lambda_DA_indices]
+    lambda_DA = DataFrame(lambda_DA_cols, col_names)
+
+    system_status_cols = [system_status_DF[:, i+1] for i in system_status_indices]
+    system_status = DataFrame(system_status_cols, col_names)
+    
+    return p_real, lambda_DA, system_status
 end
 print(scenario_generator(5))
 
@@ -37,8 +47,8 @@ function optimise_bidding_quantity(p_real, lambda_DA, system_status) # later add
     @constraint(m, t_down[t] >= 0)
     @constraint(m, p[t] >= 0)
     @constraint(m, p[t] <= 500)
-    @constraint(m, t_delta[t] = p_real[t] - p[t])
-    @constraint(m, t_delta[t] = t_up[t] - t_down[t])
+    @constraint(m, t_delta[t] == p_real[t] - p[t])
+    @constraint(m, t_delta[t] == t_up[t] - t_down[t])
 
     # One-pricing scheme
     if system_status == "deficit"
